3550af044c96de05d5d0d96115fbab28ab8cc19b
VanshSaraf <vanshsaraf2006@gmail.com>
2026-02-08 17:31:06 +0530

    Minor change

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -3,7 +3,7 @@ import prisma from "../configs/prisma.js";
 import sendEmail from "../configs/nodemailer.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "profile-marketplace" });
+export const inngest = new Inngest({ id: "profileexchange" });
 
 // Inngest Function to save user data to a database
 const syncUserCreation = inngest.createFunction(
